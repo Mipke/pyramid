9534163adab4d5ea45ef7861f1f36bb212d691c6
Jared Michel <jared@mipke.com>
2020-10-08 21:03:06 -0500

    Update font

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,5 +1,25 @@
 module.exports = {
     propsParser: require('react-docgen-typescript').withCustomConfig(
         './tsconfig.json'
-    ).parse
+    ).parse,
+    template: {
+        head: {
+            links: [
+                {
+                    rel: 'stylesheet',
+                    href: 'https://fonts.googleapis.com/css2?family=Rubik'
+                }
+            ]
+        }
+    },
+    theme: {
+        color: {
+            link: 'firebrick',
+            linkHover: 'salmon'
+        },
+        fontFamily: {
+            base: '"Rubik", "Helvetica", "Arial", sans-serif',
+            monospace: '"Rubik", "Helvetica", "Arial", sans-serif',
+        }
+    }
 };
